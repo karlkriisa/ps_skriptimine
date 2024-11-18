6dd9be6055696659fbb9abef0b2960364567271a
karlkriisa <karl-robert.kriisa@voco.ee>
2024-11-18 11:07:55 +0200

    Determine schools from CSV file and export it as CSV

---
+++ pr6.ps1
@@ -0,0 +1,15 @@
+﻿$names = Import-Csv C:\Users\Karl\ps_skriptimine\names.csv
+$result = @()
+foreach($i in $names){
+    if ([int]$i.Age -ge 4 -and [int]$i.Age -le 10){
+        $school = 'Junior'
+    } elseif ([int]$i.Age -ge 11 -and [int]$i.Age -le 17){
+        $school = 'Senior'
+    }
+    $temp = [PSCustomObject]@{
+        Name = $i.Name
+        School = $school
+    }
+    $result += $temp
+}
+$result | Export-Csv -Path C:\Users\Karl\ps_skriptimine\schools.csv -NoTypeInformation
