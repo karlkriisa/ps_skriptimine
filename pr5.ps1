aa3efb153efc62ff9e558b2bf3779d1a59b07b69
karlkriisa <karl-robert.kriisa@voco.ee>
2024-11-25 10:41:59 +0200

    Practice 5 - Where function

---
+++ pr5.ps1
@@ -0,0 +1,10 @@
+﻿Get-Process | Where {$_.ProcessName -eq 'notepad'} | Select-Object ProcessName, Id | Out-Host
+
+Get-ChildItem -Path C:\Temp\Test\ | Out-Host
+$file = Get-ChildItem -Path C:\Temp\Test | Where {$_.Name -like "*.csv"} | Select-Object Name, Length
+$KB = $file.Length/1KB
+$MB = $file.Length/1MB
+
+Write-Host "FileName: $($file.Name)"
+Write-Host "Size in KB: $KB"
+Write-Host "Size in MB: $MB"
