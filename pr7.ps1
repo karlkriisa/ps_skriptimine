1f1ea47ce94bfde056ec723dce5afb78efd6b0af
karlkriisa <karl-robert.kriisa@voco.ee>
2024-11-25 08:55:59 +0200

    Practice 7 - Get-Random

---
+++ pr7.ps1
@@ -0,0 +1,9 @@
+﻿$groups = @('Red', 'Green', 'Yellow', 'Blue')
+
+$students = @()
+for($i = 1; $i -le 20; $i++){
+    $group = $groups | Get-Random
+    $students += [PSCustomObject]@{'RollNumber'=$i; 'Group'=$group}
+}
+
+$students
