a1380e2e1f03857e35d74d3f16acbb3fd3280049
karlkriisa <karl-robert.kriisa@voco.ee>
2024-11-25 11:26:03 +0200

    Practice 12 - Functions with paramaters

---
+++ pr12.ps1
@@ -0,0 +1,22 @@
+﻿function countstart {
+    $services = Get-Service | Where {$_.Status -like 'Running'}
+    $services.Count
+}
+
+function countstop {
+    $services = Get-Service | Where {$_.Status -like 'Stopped'}
+    $services.Count
+}
+
+function countservice {
+    param (
+        [string]$status
+    )
+    $services = Get-Service | Where {$_.Status -like $status}
+    $services.Count
+}
+
+Write-Host "Total services in Running state = $(countstart)"
+Write-Host "Total services in Stopped state = $(countstop)"
+Write-Host "Total services in Running state = $(countservice -status 'Running')"
+Write-Host "Total services in Stopped state = $(countservice -status 'Stopped')"
