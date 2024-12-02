cea8525f6c9bf875cc4213cf5b4628cfacc7892e
karlkriisa <karl-robert.kriisa@voco.ee>
2024-12-02 14:27:22 +0200

    valmis yl2 - kasutaja kustutamine

---
+++ haldus/yl2_kasutajakustutamine.ps1
@@ -0,0 +1,16 @@
+﻿# Küsib kasutajalt ees- ja perenime ning
+# kustutab antud andmetega kasutaja
+
+$eesnimi = Read-Host "Sisesta oma eesnimi: "
+$perenimi = Read-Host "Sisesta oma perenimi: "
+
+$kasutajanimi = ($eesnimi.ToLower() + "." + $perenimi.ToLower())
+
+Remove-LocalUser $kasutajanimi -ErrorAction SilentlyContinue | Out-Null
+
+if($?) {
+    Write-Host "Kustutatav kasutaja on $kasutajanimi"
+}
+else{
+    Write-Host "Sellist kasutajat pole olemas!" -ForegroundColor Red
+}
