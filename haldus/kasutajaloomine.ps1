6167a7d49119a42d513077c0dc50f0e105ed265c
karlkriisa <karl-robert.kriisa@voco.ee>
2024-12-02 14:20:35 +0200

    valmis ul1, kasutaja loomine

---
+++ haldus/kasutajaloomine.ps1
@@ -0,0 +1,23 @@
+﻿# Küsib kasutajalt ees- ja perenime ning teeb
+# antud andmetega lokaalse kasutaja
+
+$eesnimi = Read-Host "Sisesta oma eesnimi: "
+$perenimi = Read-Host "Sisesta oma perenimi: "
+
+$kasutajanimi = ($eesnimi.ToLower() + "." + $perenimi.ToLower())
+$taisnimi = ($eesnimi + " " + $perenimi)
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+$kirjeldus = "Local Account"
+
+New-LocalUser $kasutajanimi `
+    -Password $parool `
+    -FullName $taisnimi `
+    -Description $kirjeldus `
+    -ErrorAction SilentlyContinue | Out-Null
+
+if($?) {
+    Write-Host "Loodav kasutaja on $kasutajanimi"
+}
+else{
+    Write-Host "Selline kasutaja on juba olemas!" -ForegroundColor Red
+}
